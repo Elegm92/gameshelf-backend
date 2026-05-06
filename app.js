0092a0cbe3d9d9207be69b3ec44d13c497c12286
Elegm92 <elegm92@gmail.com>
2026-05-06 17:07:30 +0200

    setup: conexión a PostgreSQL con Sequelize

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import sequelize from './src/config/database.js';
 dotenv.config();
 
 const app = express();
@@ -10,5 +11,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'GameShelf API funcionando' });
 });
+
+sequelize.authenticate()
+  .then(() => console.log('Base de datos conectada'))
+  .catch(err => console.error('Error conectando la base de datos:', err));
 
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
