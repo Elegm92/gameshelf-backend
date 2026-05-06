d520fd1a20f6be66d00d8e384ced9572dc36ee27
Elegm92 <elegm92@gmail.com>
2026-05-06 14:09:53 +0200

    setup:servidor express básico

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'GameShelf API funcionando' });
+});
+
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
