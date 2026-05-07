00e3da135e8e6d6ed9a28e739dc453be5505e83c
Elegm92 <elegm92@gmail.com>
2026-05-07 16:59:39 +0200

    feat: CRUD de usuarios sin auth

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './src/config/database.js';
 import './src/Models/index.js'
+import userRoutes from './src/routes/userRoute.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'GameShelf API funcionando' });
